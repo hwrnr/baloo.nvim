28fe59726a7e98db596b925013bc9003ce320e06
hwrnr <hawerner.hawerner@gmail.com>
2022-08-11 01:42:14 +0200

    Some work

--- lua/baloo/baloo.lua
+++ lua/baloo/baloo.lua
@@ -2,11 +2,15 @@ local M = {}
 function M.performSearch(query)
   -- print(getcwd())
   local cwd = vim.fn.getcwd()
-  local command = "baloosearch -d '" .. cwd .. "' '" .. query .. "'"
+  local command = "baloosearch -d '" .. cwd .. "' '" .. query .. "' 2>/dev/null"
   local handle = io.popen(command)
   local result = handle:read("*a")
   handle:close()
-  print(result)
+  local lines = vim.split(result, "\n")
+  table.remove(lines, #lines) -- Last one is empty string. There is probably a better way to do this.
+  for index, value in ipairs(lines) do
+    print(index, value)
+  end
 end
 
 return M
